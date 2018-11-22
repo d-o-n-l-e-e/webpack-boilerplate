1cac7b2d49dbe7cb164ef33222ef94e8b9617a50
donaldlee <don.lee@infor.com>
2018-11-22 01:10:58 -0500

    clean up files

--- src/app.js
+++ src/app.js
@@ -1,27 +1,28 @@
 const queryData = require('./data/test');
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
 
-import './app.scss'
+import './app.scss';
 
 document.body.classList.remove('loading');
 
 const appEl = document.getElementById('app');
 
 // append players button
-const dataBtn = document.createElement("button")
-dataBtn.textContent = 'Query Data'
-appEl.appendChild(dataBtn)
+const dataBtn = document.createElement('button');
+dataBtn.textContent = 'Query Data';
+appEl.appendChild(dataBtn);
 
 // console.log players on click
 dataBtn.onclick = () => {
-  const responseTextEl = document.createElement('div')
-  appEl.appendChild(responseTextEl)
+  const responseTextEl = document.createElement('div');
+  appEl.appendChild(responseTextEl);
 
-  queryData(function(data){
-    responseTextEl.innerHTML = '<pre>'+JSON.stringify(data, null, 2)+'</pre>';
-    console.log('data:', data)
-  })
-}
+  queryData(function(data) {
+    responseTextEl.innerHTML =
+      '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
+    console.log('data:', data);
+  });
+};
